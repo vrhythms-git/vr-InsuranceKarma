0c5cbdc53cb275e86ce66200b4fbfd1cae12f35b
vrhythms-git <65596801+vrhythms-git@users.noreply.github.com>
2021-02-09 11:01:30 +0530

    footer chart bug fixes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { CalculatedPremium, UserData } from '../store/reducers/dashboardReducer';
 import * as selectors from "../store/selectors/counterSelector";
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-footer',
@@ -17,15 +18,43 @@ export class FooterComponent implements OnInit {
 
 
   }
-  newPremium = 0;
+  //= 0;
+
+  prevHomeIns = 1100; 
+  WithoutHome = 4300;
+  chart_lifePer = parseInt(((3200 / 5400) * 100).toString()) 
+  chart_homePer = parseInt(((1100 / 5400) * 100).toString())
+  chart_autoPer = parseInt(((1100/5400) * 100).toString()) 
+
+  newPremium = 5400;
+
   ngOnInit(): void {
 
+    //this.newPremium = 0;
+    let WithoutHome = 4300;
+    
     setInterval(()=>{
+      WithoutHome = 4300;
     let subscribed = this.store.pipe(select(selectors.selectUserData)).subscribe((data) => {
      // console.log('New Premium Data from ngrx in footer is ' + JSON.stringify(data))
       if (data != undefined && data != {}) {
         try{
-        this.newPremium = data.calculatedPremium.data.newPremium;
+          // if(this.prevHomeIns !=  data.calculatedPremium.data.newPremium)
+          //     this.prevHomeIns = data.calculatedPremium.data.newPremium;
+          // else 
+              this.newPremium = (this.WithoutHome + parseInt(data.calculatedPremium.data.newPremium));
+
+              this.chart_lifePer = parseInt((( 3200 / this.newPremium) * 100).toString())
+              this.chart_homePer = parseInt((( data.calculatedPremium.data.newPremium / this.newPremium) * 100).toString())
+              this.chart_autoPer = parseInt((( 1100 / this.newPremium) * 100).toString())
+
+              $('#homeInsBar').css("width",(this.chart_homePer * 2).toString()+'%' )
+              $('#lifeInsBar').css("width",(this.chart_homePer * 2).toString()+'%' )
+              $('#autoInsBar').css("width",(this.chart_homePer * 2).toString()+'%' )
+              
+              console.log("calculated % are : chart_lifePer :" + (this.chart_lifePer) + "chart_homePer : " + (this.chart_homePer ) + " chart_autoPer:" + (this.chart_autoPer ) )
+              
+              WithoutHome = 0;
       }catch(e){
 
       }finally{
